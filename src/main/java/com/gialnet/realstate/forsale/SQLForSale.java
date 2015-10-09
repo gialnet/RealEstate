70c6d513d8fde312cc58b4761a09447a64a56d83
Usuario <Usuario@Bea.fritz.box>
2015-10-09 18:27:13 +0200

    First version SQLForSale

--- src/main/java/com/gialnet/realstate/forsale/SQLForSale.java
+++ src/main/java/com/gialnet/realstate/forsale/SQLForSale.java
@@ -6,6 +6,9 @@
 package com.gialnet.realstate.forsale;
 
 import connect.PoolConn;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.naming.NamingException;
 
@@ -14,12 +17,55 @@ import javax.naming.NamingException;
  * @author Usuario
  */
 public class SQLForSale extends PoolConn {
-     private final String version;
+     //private final String version="1.0";
 
-    public SQLForSale(String version) throws SQLException, NamingException {
+    /**
+     * 
+     * @throws SQLException
+     * @throws NamingException 
+     */
+    public SQLForSale() throws SQLException, NamingException {
         super();
-        this.version = version;
+        
     }
-     
+    
+    /**
+     * 
+     * @param xIDProperty
+     * @return
+     * @throws SQLException 
+     */
+     public TuplasForSale getTuplasForSale(int xIDProperty) throws SQLException{
+         Connection conn = PGconectar();
+         TuplasForSale Tupla = null;
+         
+      
+         try {
+          
+            PreparedStatement st = conn.prepareStatement("SELECT * from properties_for_sale where id=?");
+            st.setInt(1, xIDProperty);
+
+            ResultSet rs = st.executeQuery();
+            if (rs.next()) {
+                Tupla = new TuplasForSale.Builder(rs.getInt("id")).
+                        Owner(rs.getInt("owner")).
+                        Tipo_vivienda(rs.getString("tipo_vivienda")).
+                        build();
+            }
+            
+            
+         } catch (Exception e) {
+            
+             System.out.println("SELECT * from properties_for_sale where id=? Connection Failed!");
+             return null;
+             
+         } finally{
+             
+             conn.close();
+         }
+               
+         return Tupla;
+     }
+    
      
 }
